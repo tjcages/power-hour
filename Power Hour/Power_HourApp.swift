e5ead1ce16a1068a8a6b27bef60200783f157022
tylerj <tylerjcagle@gmail.com>
2022-11-07 12:36:53 -0700

    Initial Commit

---
+++ Power Hour/Power_HourApp.swift
@@ -0,0 +1,17 @@
+//
+//  Power_HourApp.swift
+//  Power Hour
+//
+//  Created by Tyler Cagle on 11/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Power_HourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
